9f80be945a624be6a79cab7d3479b645ea31e9e1
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-15 15:34:38 +0400

    change url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const db = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: "postgres://454ab1bb2f6c0d0f18def4e710322e46c4075c327d7c8478d62a2b4044e0b985:sk__NOW9kJF4buvSBQKoYurI@db.prisma.io:5432/postgres?sslmode=require",
+    },
+  },
+});
